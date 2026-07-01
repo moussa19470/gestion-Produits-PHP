6559acb3c56c0a08f1b36293b4f2813b331cf6b7
moussaba <moussakhady41@gmail.com>
2026-07-01 00:28:33 +0000

    initialisation de 2 categories 1er => 2 produits et 2iem 0 produits

--- index.php
+++ index.php
@@ -1,5 +1,30 @@
 <?php
+  $categories = [
 
+   0 => [
+            "code" => "C001",
+            "nom" => "categorie1",
+            "produits" => [
+                  0 => [
+                    "nom" => "produit1",
+                    "reference" => "ref1",
+                    "prix" => 3000,
+                    "quantite" => 5 
+                  ],
+                  1 => [
+                    "nom" => "produit2",
+                    "reference" => "ref2",
+                    "prix" => 2000,
+                    "quantite" => 3 
+                  ]
+            ]
+         ],
+   1 =>      [
+            "code" => "C002",
+            "nom" => "categorie2",
+            "produits" => []
+         ]
+];
 
 
 
